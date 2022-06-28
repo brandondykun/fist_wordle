f19620e4158d75658f389b674306dcd78ccd6226
Brandon Dykun <brandon.dykun@gmail.com>
2022-06-28 15:12:43 -0400

    Added useRef for input element. Changed updateWord function to only accept letters as valid input. Added useEffect to change focus to the appropriate letter input. Added handleKeyDown function to change letter input focus if the backspace key is pressed on an empty letter input. Added handleClick function to update the focus if a user clicks in a letter input box.

--- fist_wordle/src/Components/Letter.jsx
+++ fist_wordle/src/Components/Letter.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 const Letter = ({
   setWord,
@@ -7,16 +7,26 @@ const Letter = ({
   wordtoGuess,
   guessNumber,
   wordNumber,
+  currentRow,
+  currentColumn,
+  setCurrentColumn,
 }) => {
   const [letter, setLetter] = useState("");
   const [correct, setCorrect] = useState(false);
   const [inWord, setInWord] = useState(false);
 
+  const inputToFocus = useRef();
+
   const updateWord = (e) => {
-    const updatedWord = [...word];
-    updatedWord[index] = e.target.value.toUpperCase();
-    setLetter(e.target.value.toUpperCase());
-    setWord(updatedWord);
+    if (/^[a-zA-Z]+$/.test(e.target.value) || e.target.value === "") {
+      const updatedWord = [...word];
+      updatedWord[index] = e.target.value.toUpperCase();
+      setLetter(e.target.value.toUpperCase());
+      setWord(updatedWord);
+      if (e.target.value !== "" && currentColumn <= 4) {
+        setCurrentColumn(currentColumn + 1);
+      }
+    }
   };
 
   useEffect(() => {
@@ -28,15 +38,36 @@ const Letter = ({
     }
   }, [guessNumber]);
 
+  useEffect(() => {
+    if (currentRow === wordNumber && currentColumn === index + 1) {
+      inputToFocus.current.focus();
+    }
+  }, [currentRow, currentColumn]);
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Backspace") {
+      if (!letter && currentColumn > 1) {
+        setCurrentColumn(currentColumn - 1);
+      }
+    }
+  };
+
+  const handleClick = (e) => {
+    setCurrentColumn(index + 1);
+  };
+
   return (
     <>
       <input
         type="text"
+        ref={inputToFocus}
         className={`letter-input ${correct === true ? "correct" : ""} ${
           inWord ? "in-word" : ""
         }`}
         value={letter}
         onChange={updateWord}
+        onKeyDown={handleKeyDown}
+        onClick={handleClick}
         maxLength={1}
         disabled={wordNumber !== guessNumber}
       />
