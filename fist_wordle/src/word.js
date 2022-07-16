80ef72ca4a3c749cab37e0c4c2f409ef4a3cf9a1
Brandon Dykun <brandon.dykun@gmail.com>
2022-07-16 11:36:35 -0400

    Created separate file to keep the answer word and message to simplify updating.

---
+++ fist_wordle/src/word.js
@@ -0,0 +1,5 @@
+export const answer = {
+  word: ["J", "O", "K", "E", "R"],
+  message:
+    "Brevity Term: Fuel state above BINGO at which separation or BUGOUT or event termination should begin.",
+};
